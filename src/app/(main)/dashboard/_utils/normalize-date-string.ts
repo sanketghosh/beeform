986079022621dd132a718639b03faa2192633991
sanket <code.sanket13@gmail.com>
2025-04-28 04:14:31 +0530

    add: using enGB for fixing date parse error

--- src/app/(main)/dashboard/_utils/normalize-date-string.ts
+++
@@ -1,5 +0,0 @@
-export function normalizeDateString(dateString: string) {
-  const [day, month, year] = dateString.split("/");
-  if (!day || !month || !year) return null;
-  return `${day.padStart(2, "0")}/${month.padStart(2, "0")}/${year}`;
-}
